dfaae4ade6bd244a856778060577a84a2cb3b0a9
Chris Moreton <chris@netsensia.com>
2020-10-21 16:21:49 +0100

    Add two more containers

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -1,5 +1,23 @@
 resource "aws_ecr_repository" "rivalchess_player" {
-  name                 = "rivalchess-player"
+  name                 = "rivalchess-vie-player"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "rivalchess_recorder" {
+  name                 = "rivalchess-vie-recorder"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "rivalchess_generator" {
+  name                 = "rivalchess-vie-generator"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
